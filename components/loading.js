91bcb4c521cbd19248c573a00537eb6f48c0d683
Peter Yang <ipod4928@gmail.com>
2021-07-04 22:28:37 +0800

    Change img to Image and using local svg file for logo

--- components/loading.js
+++ components/loading.js
@@ -1,14 +1,16 @@
 import {Circle} from "better-react-spinkit"
+import Image from "next/image"
 
 function Loading() {
   return (
     <center style={{display: 'grid', placeItems: 'center', height:'100vh'}}>
       <div>
-        <img
-          src='https://seeklogo.com/images/W/whatsapp-icon-logo-6E793ACECD-seeklogo.com.png'
+        <Image
+          src='/icon.svg'
           alt=''
           style={{ marginBottom: 10 }}
           height={200}
+          width={200}
         />
         <Circle color="#3CBC2B" size={60}/>
       </div>
